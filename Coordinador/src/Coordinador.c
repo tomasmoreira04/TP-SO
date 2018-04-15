50b196de9c490257d0773353fd0450f7d7e5da65
FNLucero <facundo.lucero@outlook.es>
2018-04-15 01:30:10 -0300

    coordinador server

--- Coordinador/src/Coordinador.c
+++ Coordinador/src/Coordinador.c
@@ -1,25 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <netdb.h>
+#include  <unistd.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include "../../Librerias/src/Socket.c"
-#include "commons/string.h"
 
+#define PORT 9034
 void main() {
-	char * j=malloc(50);
-	int socketServer=conexionConServidor("127.0.0.1","9034");
-	while(1){
-		int tamano=strlen(j);
-		recv(socketServer,&tamano,sizeof(int),0);
-		recv(socketServer,j,tamano,0);
+	int listener;
+	char buf[256];
+	int nbytes;
 
-		printf("\nel valor %s\n", j);
+	int i, j;
+	FD_ZERO(&master);
+	FD_ZERO(&read_fds);
+
+	listener = crearSocketDeEscucha(PORT);
+
+	FD_SET(listener, &master);
+	fdmax = listener;
+	while (1) {
+		read_fds = master;
+		if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
+			perror("select");
+			exit(1);
+		}
+		for (i = 3; i <= fdmax; i++) {
+			if (FD_ISSET(i, &read_fds)) {
+				if (i == listener) {
+					aceptarNuevaConexion(listener);
+				} else {
+					int tamano;
+					recv(i,&tamano,sizeof(int),0);
+
+					printf(" tamano:   %d\n  ",tamano);
+
+
+					if ((nbytes = recv(i, buf, tamano, 0)) <= 0) {
+						if (nbytes == 0) {
+							printf("selectserver: socket %d hung up\n", i);
+						} else {
+							perror("recv");
+						}
+						close(i);
+						FD_CLR(i, &master);
+					} else {
+						printf("me llego %s\n", buf);
+						for (j = 3; j <= fdmax; j++) {
+							if (FD_ISSET(j, &master)) {
+								if (j != listener && j != i) {
+									int tamanoEnviar=strlen(buf);
+
+									send(j,&tamanoEnviar,sizeof(int),0);
+
+									if (send(j, buf, tamanoEnviar, 0) == -1) {
+										perror("send");
+									}
+								}
+							}
+						}
+						//consola planif
+					}
+				}
+			}
+		}
 	}
 }
 
