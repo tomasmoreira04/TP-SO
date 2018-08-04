82753fb7064fdce7eecfa620308b2e6d1a07ca9d
FNLucero <facundo.lucero@outlook.es>
2018-08-04 01:18:14 -0300

    Ivan es el MANDELA del siglo 21

--- putty.sh
+++ putty.sh
@@ -1,4 +1,6 @@
-echo "Holi WACHO"
+echo "WELCOME TO SOMALIA"
+
+
 cd ../
 echo "Instalando Commons"
 git clone https://github.com/sisoputnfrba/so-commons-library.git
@@ -10,4 +12,31 @@ git clone https://github.com/sisoputnfrba/parsi.git
 cd parsi
 sudo make install
 echo "Compilando"
+
+
+cd /home/utnso/tp-2018-1c-PMD/ESI/Debug
+rm ESI
+make ESI
+
+cd ../../Bibliotecas/Debug
+rm Bibliotecas
+make Bibliotecas
+
+cd ../../Instancia/Debug
+rm Instancia
+make Instancia
+
+cd ../../Planificador/Debug
+rm Planificador
+make Planificador
+
+cd ../../Coordinador/Debug
+rm Coordinador
+make Coordinador
+
 chmod 0777 *
+
+
+
+
+
