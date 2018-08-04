347e41eea57af2c4edf1b08d62cd497cf28fd9f8
Bruno Tonelli <brunotonelli97@gmail.com>
2018-08-04 08:05:49 -0300

    debug

---
+++ Coordinador/Debug/src/Coordinador.d
@@ -0,0 +1,30 @@
+src/Coordinador.d: ../src/Coordinador.c \
+ ../src/../../Bibliotecas/src/Socket.c \
+ ../src/../../Bibliotecas/src/Socket.h \
+ ../src/../../Bibliotecas/src/Estructuras.h \
+ ../src/../../Bibliotecas/src/Macros.h \
+ ../src/../../Bibliotecas/src/Configuracion.c \
+ ../src/../../Bibliotecas/src/Color.h \
+ ../src/../../Bibliotecas/src/Configuracion.h \
+ ../src/../../Bibliotecas/src/Estructuras.h \
+ ../src/../../Bibliotecas/src/Color.h ../src/Coordinador.h
+
+../src/../../Bibliotecas/src/Socket.c:
+
+../src/../../Bibliotecas/src/Socket.h:
+
+../src/../../Bibliotecas/src/Estructuras.h:
+
+../src/../../Bibliotecas/src/Macros.h:
+
+../src/../../Bibliotecas/src/Configuracion.c:
+
+../src/../../Bibliotecas/src/Color.h:
+
+../src/../../Bibliotecas/src/Configuracion.h:
+
+../src/../../Bibliotecas/src/Estructuras.h:
+
+../src/../../Bibliotecas/src/Color.h:
+
+../src/Coordinador.h:
